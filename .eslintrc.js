32341d206ae7ebab84df945b42d11523517625a1
Yusuke Wada <wada.yusuke@classmethod.jp>
2020-12-10 10:24:01 +0900

    fix formatter

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
     ],
     env: {
         node: true,
